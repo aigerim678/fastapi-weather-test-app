ed2ee1e3c2af264ebffd565873ad0f3019b3cfe7
aigerim678 <aigerimsabyrkhan393@gmail.com>
2024-12-14 23:43:28 +0500

    fix path

--- app/main.py
+++ app/main.py
@@ -1,3 +1,5 @@
+import sys
+import os
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
 import uvicorn
@@ -7,6 +9,9 @@ from core.models import db_helper
 from core.config import settings
 from api import router
 
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(current_dir)
+sys.path.append(parent_dir)
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
